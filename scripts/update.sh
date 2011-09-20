duzailian/google-clang-tools

commit 0b889cf83372d9d7375ac3ef332653e857ac553f

Download clang-xxx.tgz into /tmp instead of the current directory.
BUG=96722
TEST=none


Review URL: http://codereview.chromium.org/7931022

git-svn-id: http://src.chromium.org/svn/trunk/src/tools/clang@101931 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

scripts/update.sh
@@ -110,18 +110,23 @@ if [ -z "$force_local_build" ]; then
   # and goma relies on having matching binary hashes on client and server too.
   CDS_URL=https://commondatastorage.googleapis.com/chromium-browser-clang
   CDS_FILE="clang-${CLANG_REVISION}.tgz"
+  CDS_OUT_DIR=$(mktemp -d -t clang_download.XXXXXX)
+  CDS_OUTPUT="${CDS_OUT_DIR}/${CDS_FILE}"
   echo Trying to download prebuilt clang
   if [ "${OS}" = "Linux" ]; then
-    wget "${CDS_URL}/Linux_x64/${CDS_FILE}" || rm -f "${CDS_FILE}"
+    wget "${CDS_URL}/Linux_x64/${CDS_FILE}" -O "${CDS_OUTPUT}" || \
+        rm -f "${CDS_OUT_DIR}"
   elif [ "${OS}" = "Darwin" ]; then
-    curl -L --fail -O "${CDS_URL}/Mac/${CDS_FILE}" || rm -f "${CDS_FILE}"
+    curl -L --fail "${CDS_URL}/Mac/${CDS_FILE}" -o "${CDS_OUTPUT}" || \
+        rm -f "${CDS_OUT_DIR}"
   fi
-  if [ -f "${CDS_FILE}" ]; then
+  if [ -f "${CDS_OUTPUT}" ]; then
     rm -rf "${LLVM_BUILD_DIR}/Release+Asserts"
     mkdir -p "${LLVM_BUILD_DIR}/Release+Asserts"
-    tar -xzf "${CDS_FILE}" -C "${LLVM_BUILD_DIR}/Release+Asserts"
+    tar -xzf "${CDS_OUTPUT}" -C "${LLVM_BUILD_DIR}/Release+Asserts"
     echo clang "${CLANG_REVISION}" unpacked
     echo "${CLANG_REVISION}" > "${STAMP_FILE}"
+    rm -rf "${CDS_OUT_DIR}"
     exit 0
   else
     echo Did not find prebuilt clang at r"${CLANG_REVISION}", building

commit 00692c54f5784e39613b1ab779eb3f2a6fb54f83

clang updater: Fix error-case regression introduced in r101931
BUG=96722
TEST=none


Review URL: http://codereview.chromium.org/7983009

git-svn-id: http://src.chromium.org/svn/trunk/src/tools/clang@101998 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

scripts/update.sh
@@ -115,10 +115,10 @@ if [ -z "$force_local_build" ]; then
   echo Trying to download prebuilt clang
   if [ "${OS}" = "Linux" ]; then
     wget "${CDS_URL}/Linux_x64/${CDS_FILE}" -O "${CDS_OUTPUT}" || \
-        rm -f "${CDS_OUT_DIR}"
+        rm -rf "${CDS_OUT_DIR}"
   elif [ "${OS}" = "Darwin" ]; then
     curl -L --fail "${CDS_URL}/Mac/${CDS_FILE}" -o "${CDS_OUTPUT}" || \
-        rm -f "${CDS_OUT_DIR}"
+        rm -rf "${CDS_OUT_DIR}"
   fi
   if [ -f "${CDS_OUTPUT}" ]; then
     rm -rf "${LLVM_BUILD_DIR}/Release+Asserts"