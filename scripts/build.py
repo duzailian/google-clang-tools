f2e1fa017ad50fe5864e59c50d925add9b5b2c91
Hans Wennborg <hans@chromium.org>
2020-01-24 22:31:29 +0000

    Revert "Roll clang n333938-3a6da112-1 : n340158-efb130fc-2."
    This reverts commit 0aa05cca9db25a9fa8d18f77bf6b2452bc608ead.
    
    Reason for revert:
    This broke arm64-builder-rel and some other builders, see bug.
    
    Original change's description:
    > Roll clang n333938-3a6da112-1 : n340158-efb130fc-2.
    > 
    > Ran `tools/clang/scripts/upload_revision.py efb130fc93059bdf02d3a83950ddabc5d119f9de`.
    > 
    > This increases the clang version from 10 to 11.
    > 
    > It also disables the use of vector instructions in zlib to work around
    > new false positives in msan (see crbug.com/1045291).
    > 
    > Bug: 1030664
    > Change-Id: I5417d3063f0d67a61cdf31141b05a51a56e84482
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014904
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Hans Wennborg <hans@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#735080}
    
    TBR=thakis@chromium.org,hans@chromium.org
    
    Change-Id: I48754484c26ebf6b1f0b4a47c82fe3c74ee39837
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1030664,1045592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020785
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#735124}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: ec2aab7d123590cfb332ba56d7faee2e1ff4a779

--- scripts/build.py
+++ scripts/build.py
@@ -338,6 +338,15 @@ def main():
                       default=sys.platform in ('linux2', 'darwin'))
   args = parser.parse_args()
 
+  # TODO(crbug.com/1042192): Remove in the next Clang roll.
+  if args.llvm_force_head_revision:
+    global RELEASE_VERSION
+    RELEASE_VERSION = '11.0.0'
+    old_lib_dir = os.path.join(LLVM_BUILD_DIR, 'lib', 'clang', '10.0.0')
+    if (os.path.isdir(old_lib_dir)):
+      print('Removing old lib dir: ' + old_lib_dir)
+      RmTree(old_lib_dir)
+
   if args.lto_lld and not args.bootstrap:
     print('--lto-lld requires --bootstrap')
     return 1
@@ -451,8 +460,6 @@ def main():
                      '-DCOMPILER_RT_USE_LIBCXX=NO',
                      # Don't run Go bindings tests; PGO makes them confused.
                      '-DLLVM_INCLUDE_GO_TESTS=OFF',
-                     # TODO(b/148147812) Goma client doesn't handle in-process cc1.
-                     '-DCLANG_SPAWN_CC1=ON',
                      ]
 
   if args.gcc_toolchain:
