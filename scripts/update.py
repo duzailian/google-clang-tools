7d0befed10cd7a60981375046b8198b941daf455
Nico Weber <thakis@chromium.org>
2020-03-06 15:24:28 +0000

    Roll clang n343707-61f538d3-1 : n344329-9284abd0-1.
    Ran `./tools/clang/scripts/upload_revision.py 9284abd0040afecfd619dbcf1b244a8b533291c9`.
    
    linux_chromium_chromeos_msan_rel_ng trybot redness: Also red on the
    main waterfall in similar ways and has been for days (!).
    
    ipad-device, iphone-device trybot redness: happens on whitespace cl too, filed crbug.com/1059256
    
    Bug: 1058012
    Cq-Include-Trybots: chromium/try:mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_asan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg,win-asan
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:linux_chromium_compile_dbg_32_ng
    Cq-Include-Trybots: chromium/try:win7-rel,win-angle-deqp-rel-32
    Cq-Include-Trybots: chromium/try:win-angle-deqp-rel-64
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Change-Id: Iafc17a96a7d40e9b99f58bce474ed50dc6bfb0da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2086341
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#747705}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 2b76f3eac4172b1fbf1cfce55e6637f9ff3478f0

--- scripts/update.py
+++ scripts/update.py
@@ -39,8 +39,8 @@ import zipfile
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://chromium.googlesource.com/chromium/src/+/master/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION = '61f538d37b80eb589f8990f0b4513d702704ea38'
-CLANG_SVN_REVISION = 'n343707'
+CLANG_REVISION = '9284abd0040afecfd619dbcf1b244a8b533291c9'
+CLANG_SVN_REVISION = 'n344329'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s-%s' % (CLANG_SVN_REVISION, CLANG_REVISION[:8],
