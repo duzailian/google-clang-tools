6cbea33c97d07ee51a1e03ee02b9d9c3d919752a
Nico Weber <thakis@chromium.org>
2020-07-11 00:21:58 +0000

    Roll clang n358615-fb1aa286-3 : n359864-04b9a46c-1.
    Ran `./tools/clang/scripts/upload_revision.py 04b9a46c842f793a2baedcad64de35fcbd3e93b7`.
    
    TBR=hans
    
    Bug: 1101237
    Change-Id: I39243368a5c578fb392814e0fbe654faf0615e6a
    Cq-Include-Trybots: chromium/try:mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_asan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg,win-asan
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:linux_chromium_compile_dbg_32_ng
    Cq-Include-Trybots: chromium/try:win7-rel,win-angle-deqp-rel-32
    Cq-Include-Trybots: chromium/try:linux_angle_deqp_rel_ng
    Cq-Include-Trybots: chromium/try:win-angle-deqp-rel-64
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,mac-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291352
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#787451}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 54d234ac48987230c084ab4742e98de878514890

--- scripts/update.py
+++ scripts/update.py
@@ -37,9 +37,9 @@ import zipfile
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://chromium.googlesource.com/chromium/src/+/master/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION = 'fb1aa286c1400ad7'
-CLANG_SVN_REVISION = 'n358615'
-CLANG_SUB_REVISION = 3
+CLANG_REVISION = '04b9a46c842f793a2baedcad64de35fcbd3e93b7'
+CLANG_SVN_REVISION = 'n359864'
+CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s-%s' % (CLANG_SVN_REVISION, CLANG_REVISION[:8],
                                 CLANG_SUB_REVISION)
