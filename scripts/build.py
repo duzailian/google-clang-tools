708cbfda8bd0686e0f302f54f1b85896cd5881a9
Hans Wennborg <hans@chromium.org>
2020-08-25 08:48:24 +0000

    Clang build.py: Update gnuwin version to 14
    which includes 'printenv' now used by some lit test.
    
    The zip was constructed like this:
    
    $ gsutil.py cp gs://chromium-browser-clang/tools/gnuwin-13.zip .
    $ unzip gnuwin-13.zip
    $ curl -L -O https://downloads.sourceforge.net/project/gnuwin32/coreutils/5.3.0/coreutils-5.3.0-bin.zip
    $ unzip coreutils-5.3.0-bin.zip bin/printenv.exe
    $ mv bin/printenv.exe gnuwin/
    $ zip -r9 gnuwin-14.zip gnuwin/
    $ gsutil.py cp -n -a public-read gnuwin-14.zip gs://chromium-browser-clang/tools/gnuwin-14.zip
    
    TBR=thakis
    
    Bug: 1121387
    Change-Id: I93f49303e104431be50389bbf7202621f6881d76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2374146
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801313}
    GitOrigin-RevId: 5d36606dcbc6dcae21003b862879a71c0848402f

--- scripts/build.py
+++ scripts/build.py
@@ -253,7 +253,7 @@ def AddGnuWinToPath():
     return
 
   gnuwin_dir = os.path.join(LLVM_BUILD_TOOLS_DIR, 'gnuwin')
-  GNUWIN_VERSION = '13'
+  GNUWIN_VERSION = '14'
   GNUWIN_STAMP = os.path.join(gnuwin_dir, 'stamp')
   if ReadStampFile(GNUWIN_STAMP) == GNUWIN_VERSION:
     print('GNU Win tools already up to date.')
